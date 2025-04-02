c99228e5a64851db93ee87a303d9d1935df48276
Adrian Burca <burca.a50@gmail.com>
2025-04-02 03:18:54 +0300

    am creat mapa si movement la caracter

--- main.c
+++ main.c
@@ -1,46 +1,168 @@
 #include "raylib.h"
-#include "menu.h"
-#include <stdio.h>
+#include "raymath.h"
 
-int main() {
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    InitWindow(screenWidth, screenHeight, "Farm Sim");
+#define TILE_WIDTH 64
+#define TILE_HEIGHT 64
 
-    int selectedOption = MENU_START_GAME;
-    bool inMenu = true;
+void GameStartup();
+void GameUpdate();
+void GameRender();
+void GameShutDown();
+void DrawTile(int pos_x, int pos_y, int texture_index_x, int texture_index_y);
 
-    SetTargetFPS(60);
+const int screenWidth = 800;
+const int screenHeight = 600;
 
-    while (!WindowShouldClose()) {
-        if (inMenu) {
-            BeginDrawing();
-            DrawMenu(selectedOption);
-            EndDrawing();
+#define MAX_TEXTURES 2
 
-            MenuOption result = UpdateMenu(selectedOption);
-            if (result != MENU_NONE) {
-                switch (result) {
-                    case MENU_START_GAME:
-                        inMenu = false;
-                        break;
-                    case MENU_OPTIONS:
-                        break;
-                    case MENU_EXIT:
-                        CloseWindow();
-                        return 0;
-                    default:
-                        break;
-                }
-            }
-        } else {
-            BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Jocul a început!", 300, 250, 30, BLACK);
-            EndDrawing();
+typedef enum {
+    TEXTURE_TILEMAP = 0
+} texture_asset;
+
+Texture2D textures[MAX_TEXTURES];
+
+#define WORLD_WIDTH 25
+#define WORLD_HEIGHT 25
+
+typedef struct {
+    int x;
+    int y;
+} sTile;
+
+sTile world[WORLD_WIDTH][WORLD_WIDTH];
+
+Camera2D camera = { 0 };
+
+typedef struct {
+    int x;
+    int y;
+} sEntity;
+
+sEntity player;
+
+void GameStartup() {
+
+    InitAudioDevice();
+
+    Image image = LoadImage("textures/mapPack_spritesheet.png");
+    textures[TEXTURE_TILEMAP] = LoadTextureFromImage(image);
+    UnloadImage(image);
+
+    for (int i = 0; i < WORLD_WIDTH; i++) {
+        for (int j = 0; j < WORLD_HEIGHT; j++) {
+            world[i][j] = (sTile)
+            {
+                .x = i,
+                .y = j,
+            };
         }
     }
 
-    CloseWindow();
+    player = (sEntity)
+    {
+        .x = TILE_WIDTH * 3,
+        .y = TILE_HEIGHT * 3
+    };
+
+    camera.target = (Vector2){player.x, player.y};
+    camera.offset = (Vector2){(float)screenWidth / 2, (float)screenHeight / 2};
+    camera.rotation = 0.0f;
+    camera.zoom = 2.0f;
+
+}
+void GameUpdate() {
+
+    float x = player.x;
+    float y = player.y;
+    if (IsKeyPressed(KEY_A)) {
+        x -= 1 * TILE_WIDTH;
+    } else if (IsKeyPressed(KEY_D)) {
+        x += 1 * TILE_WIDTH;
+    } else if (IsKeyPressed(KEY_W)) {
+        y -= 1 * TILE_HEIGHT;
+    } else if (IsKeyPressed(KEY_S)) {
+        y += 1 * TILE_HEIGHT;
+    }
+
+    float wheel = GetMouseWheelMove();
+    if (wheel != 0) {
+        const float zoomIncrement = 0.125f;
+        camera.zoom += (wheel * zoomIncrement);
+        if (camera.zoom < 0.7f) camera.zoom = 0.7f;
+        if (camera.zoom > 3.0f) camera.zoom = 3.0f;
+    }
+
+    player.x = x;
+    player.y = y;
+
+    camera.target = (Vector2){ player.x, player.y};
+
+}
+void GameRender() {
+
+    BeginMode2D(camera);
+
+    sTile tile;
+    int texture_index_x = 0;
+    int texture_index_y = 0;
+    for (int i = 0; i < WORLD_WIDTH; i++) {
+        for (int j = 0; j < WORLD_HEIGHT; j++) {
+            tile = world[i][j];
+            texture_index_x = 7;
+            texture_index_y = 7;
+
+            DrawTile(tile.x * TILE_WIDTH, tile.y * TILE_HEIGHT, texture_index_x, texture_index_y);    
+        }
+    }
+
+    DrawTile(camera.target.x, camera.target.y, 1, 8);
+
+    EndMode2D();
+
+    //DrawRectangle(5, 5, 330, 120, Fade(SKYBLUE, 0.5f));
+    //DrawRectangleLines(5, 5, 330, 120, BLUE);
+
+    //DrawText(TextFormat("Camera Target: (%06.2f, %06.2f)", camera.target.x, camera.target.y), 15, 10, 14, YELLOW);
+    //DrawText(TextFormat("Camera Zoom: %06.2f", camera.zoom), 15, 30, 14, YELLOW);
+
+}
+void GameShutDown() {
+
+    for (int i = 0; i < MAX_TEXTURES; i++) {
+        UnloadTexture(textures[i]);
+    }
+
+    CloseAudioDevice();
+}
+
+void DrawTile(int pos_x, int pos_y, int texture_index_x, int texture_index_y) {
+    Rectangle source = { (float)texture_index_x * TILE_WIDTH, (float)texture_index_y * TILE_HEIGHT, (float)TILE_WIDTH, (float)TILE_HEIGHT };
+    Rectangle dest  = { (float)(pos_x), (float)(pos_y), (float)TILE_WIDTH, (float)TILE_HEIGHT};
+    Vector2 origin = {0, 0};
+    DrawTexturePro(textures[TEXTURE_TILEMAP], source, dest, origin, 0.0f, WHITE);
+}
+
+int main(void) {
+
+    InitWindow(screenWidth, screenHeight, "Farm Sim");
+    SetTargetFPS(60);
+
+    GameStartup();
+    while(!WindowShouldClose())
+    {
+        GameUpdate();
+
+        BeginDrawing();
+        ClearBackground(GRAY);
+
+        GameRender();
+
+        EndDrawing();
+    }
+
+    GameShutDown();
+
+    CloseWindow();  
+
     return 0;
 }
---
+++ makefile
@@ -0,0 +1,34 @@
+# Compilator
+CC = gcc
+
+# Opțiuni de compilare
+CFLAGS = -Wall
+
+# Biblioteci pentru linker
+LDLIBS = -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+
+# Numele executabilului
+TARGET = prog
+
+# Fișierele sursă
+SRCS = main.c
+
+# Fișierele obiect (generate automat)
+OBJS = $(SRCS:.c=.o)
+
+# Regula implicită
+all: $(TARGET)
+
+# Legarea executabilului
+$(TARGET): $(OBJS)
+	$(CC) -o $@ $(OBJS) $(LDLIBS)
+
+# Compilare fișiere sursă în obiecte
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+# Curățare
+clean:
+	rm -f $(OBJS) $(TARGET)
+
+.PHONY: all clean
